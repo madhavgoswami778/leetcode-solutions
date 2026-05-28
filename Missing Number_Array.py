63b3bead5a2dd2600a7a60e25d3618d493b661c8
Madhav Goswami <149584915+madhavgoswami778@users.noreply.github.com>
2026-05-28 21:03:31 +0530

    Create Missing Number_Array.py

---
+++ Missing Number_Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+
+        return expected_sum - actual_sum
+
+
+# Example Usage
+nums = [3, 0, 1]
+
+sol = Solution()
+print(sol.missingNumber(nums))
